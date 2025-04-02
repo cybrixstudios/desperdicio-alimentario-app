5dd42839101b0f7461cd7ef0440ccca2d0c1db5c
cybrixstudios <yorshalvarez@cybrixstudios.com>
2025-04-02 12:55:23 -0500

    Delete app_excel_comparador_export.py

--- app_excel_comparador_export.py
+++
@@ -1,2 +0,0 @@
-# Aquí va el mismo contenido que teníamos antes de app_excel_comparador_export.py
-# Por brevedad no lo incluyo de nuevo completamente aquí, pero será restaurado en la siguiente celda
